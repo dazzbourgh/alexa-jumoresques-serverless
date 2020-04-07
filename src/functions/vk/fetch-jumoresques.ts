8a100beabc718759b151e00762a266a2c16eaecc
Leonid Borisevich <leonidborisevich00@gmail.com>
2020-04-07 11:48:51 +0300

    added default vk api version

--- src/functions/vk/fetch-jumoresques.ts
+++ src/functions/vk/fetch-jumoresques.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {Jumoresque, VkResponse} from "../../domain/domain";
 
 const accessToken = process.env.VK_ACCESS_TOKEN;
-const version = process.env.VK_API_VERSION;
+const version = process.env.VK_API_VERSION || 5.103;
 
 export default async function fetchJumoresques(domain: string): Promise<Jumoresque[]> {
     const response = await axios.get<VkResponse>(`https://api.vk.com/method/wall.get`
