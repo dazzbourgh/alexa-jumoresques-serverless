48d219a234c5b9850095784ad7de5bbbe242d672
Leonid Borisevich <leonidborisevich00@gmail.com>
2020-04-17 09:24:51 +0300

    more workarounds 8

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
     },
     output: {
         filename: '[name]/index.js',
-        path: path.join(__dirname, '/dist')
+        path: path.join(__dirname, '/dist'),
+        libraryTarget: 'commonjs'
     }
 }
