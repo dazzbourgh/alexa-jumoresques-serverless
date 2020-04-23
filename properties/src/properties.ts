ef7261f0fd21f1387bec5655136e0a9a26150c2e
Leonid Borisevich <leonidborisevich00@gmail.com>
2020-04-23 10:55:28 +0300

    rollback moduling

--- properties/src/properties.ts
+++
@@ -1,6 +0,0 @@
-import prepareProperties from './prepare-properties'
-
-// @ts-ignore
-const environment = props[process.env.ENVIRONMENT]
-
-export const properties = (file: any) => prepareProperties(Object.assign(JSON.parse(JSON.stringify(file.default)), environment))
---
+++ src/properties/properties.ts
@@ -0,0 +1,9 @@
+import props from './env.json'
+import prepareProperties from './prepare-properties'
+
+const def = JSON.parse(JSON.stringify(props.default))
+
+// @ts-ignore
+const environment = props[process.env.ENVIRONMENT]
+
+export const properties = prepareProperties(Object.assign(def, environment))
