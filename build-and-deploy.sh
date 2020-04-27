3425372d525124182f16168ce4990b9775c2c035
Leonid Borisevich <leonidborisevich00@gmail.com>
2020-04-27 04:11:58 +0300

    fixed broken policy and added cleanup to local deployment script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -8,3 +8,4 @@ mkdir common_layer/nodejs
 cp -RL node_modules common_layer/nodejs
 aws cloudformation package --template-file infrastructure/template.yml --s3-bucket jumoresques-lambda-bucket --output-template-file packaged-template.yml
 aws cloudformation deploy --template-file packaged-template.yml --stack-name alexa-jumoresques-serverless-prod-stack --region us-west-1 --capabilities CAPABILITY_IAM --parameter-overrides Env=production
+rm -r common_layer
