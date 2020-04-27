e9207ae6279acec146b12041747fdc6751fc0e80
Leonid Borisevich <leonidborisevich00@gmail.com>
2020-04-27 04:06:10 +0300

    added script for local build and permissions to get secret value for refresh function

---
+++ build-and-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm install
+npm run build
+find ./node_modules -type f -exec touch {} +
+mkdir common_layer
+mkdir common_layer/nodejs
+cp -RL node_modules common_layer/nodejs
+aws cloudformation package --template-file infrastructure/template.yml --s3-bucket jumoresques-lambda-bucket --output-template-file packaged-template.yml
+aws cloudformation deploy --template-file packaged-template.yml --stack-name alexa-jumoresques-serverless-prod-stack --region us-west-1 --capabilities CAPABILITY_IAM --parameter-overrides Env=production
