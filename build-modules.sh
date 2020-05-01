eef3c32887e44d5807075fd75f3b28e49b307365
Leonid Borisevich <leonidborisevich00@gmail.com>
2020-05-02 01:15:47 +0300

    finished decomposing

--- build-modules.sh
+++ build-modules.sh
@@ -2,12 +2,16 @@
 
 for dir in "src/layers"/*; do
     if test -d "$dir"; then
+        find "$dir"/nodejs/node_modules -type f -exec touch {} +
+        npm i --prefix "$dir"/nodejs
         npm run build --prefix "$dir"/nodejs
     fi
 done
 
 for dir in "src/handlers"/*; do
     if test -d "$dir"; then
+        find "$dir"/node_modules -type f -exec touch {} +
+        npm i --prefix "$dir"
         npm run build --prefix "$dir"
     fi
 done
---
+++ src/handlers/yandex/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    globals: {
+        "ts-jest": {
+            tsConfig: "tsconfig.json"
+        }
+    },
+    moduleFileExtensions: [
+        "ts",
+        "js"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    testMatch: [
+        "<rootDir>/src/**/*.test.(ts|js)",
+        "<rootDir>/test/**/*.test.(ts|js)"
+    ],
+    testEnvironment: "node"
+};
